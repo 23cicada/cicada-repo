4bddf6324a793588b0baf15d30e7fe7998790469
cicada-c <hey.changjun@gmail.com>
2024-12-12 14:35:52 +0800

    test eslint

--- packages/eslint-config/eslint-config-next.mjs
+++ packages/eslint-config/eslint-config-next.mjs
@@ -1,21 +1,22 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import eslintConfigBase from "./eslint-config-base.mjs";
 import { FlatCompat } from '@eslint/eslintrc'
-import { fixupConfigRules } from "@eslint/compat";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginTurbo from "eslint-config-turbo/flat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log('baseDirectory', import.meta.dirname ?? __dirname)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    // resolvePluginsRelativeTo: __dirname,
+    baseDirectory: __dirname
 })
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    ...eslintConfigBase,
-    // https://nextjs.org/docs/app/api-reference/config/eslint#additional-configurations
-    ...compat.extends('next'),
+    pluginJs.configs.recommended,
+    eslintConfigPrettier,
+    ...pluginTurbo,
+    // https://github.com/vercel/next.js/issues/64114#issuecomment-2440625243
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
 ];
