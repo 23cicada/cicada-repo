ba7567c2125da6c534244724d89d4cbbacf2940b
cicada <hey.changjun@gmail.com>
2024-12-17 23:46:58 +0800

    Writing the firstValues type

--- apps/odin-nodejs/src/server.ts
+++ apps/odin-nodejs/src/server.ts
@@ -1,20 +1,21 @@
+/* eslint-disable */
 // https://www.typescriptlang.org/docs/handbook/modules/reference.html#export--and-import--require
 import http from 'node:http'
-import fs from "node:fs/promises"
-import path from "node:path"
+import fs from 'node:fs/promises'
+import path from 'node:path'
 import formidable from 'formidable'
-import { firstValues } from "formidable/src/helpers/firstValues.js"
-
-// type Test = Record<string, number[]>
-// type ExtractedType = Test[keyof Test][number];
+import { firstValues } from 'formidable/src/helpers/firstValues.js'
 
 const routes: Record<string, http.RequestListener> = {
-  'GET /hello-world': (request: http.IncomingMessage, response: http.ServerResponse) => {
+  'GET /hello-world': (
+    request: http.IncomingMessage,
+    response: http.ServerResponse,
+  ) => {
     const { headers, method, url } = request
     const responseBody = { headers, method, url }
 
     response.writeHead(200, {
-      "Content-Type": "text/html",
+      'Content-Type': 'text/html',
     })
 
     response.end(`
@@ -26,35 +27,47 @@ const routes: Record<string, http.RequestListener> = {
       </html>
     `)
   },
-  'POST /echo': (request: http.IncomingMessage, response: http.ServerResponse) => {
+  'POST /echo': (
+    request: http.IncomingMessage,
+    response: http.ServerResponse,
+  ) => {
     request.pipe(response)
   },
-  'GET /read-file': async (request: http.IncomingMessage, response: http.ServerResponse) => {
+  'GET /read-file': async (
+    request: http.IncomingMessage,
+    response: http.ServerResponse,
+  ) => {
     const content = await fs.readFile(path.resolve(__dirname, './test.json'))
     response.setHeader('Content-type', 'application/json')
     response.end(content)
   },
-  'POST /write-file': async (request: http.IncomingMessage, response: http.ServerResponse) => {
+  'POST /write-file': async (
+    request: http.IncomingMessage,
+    response: http.ServerResponse,
+  ) => {
     try {
       const pathname = path.resolve(__dirname, './test.json')
       const body = await getRequestBody(request)
       const existing = await fs.readFile(pathname, 'utf-8')
       const content = {
         ...JSON.parse(existing),
-        ...JSON.parse(body.toString())
+        ...JSON.parse(body.toString()),
       }
       await fs.writeFile(pathname, JSON.stringify(content))
-      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.writeHead(200, { 'Content-Type': 'application/json' })
       response.end(JSON.stringify({ success: true }))
     } catch (err) {
-      response.writeHead(500, { 'Content-Type': 'application/json' });
+      response.writeHead(500, { 'Content-Type': 'application/json' })
       response.end(JSON.stringify({ success: false, err }))
     }
   },
-  'GET /upload': async (request: http.IncomingMessage, response: http.ServerResponse) => {
+  'GET /upload': async (
+    request: http.IncomingMessage,
+    response: http.ServerResponse,
+  ) => {
     const pathname = path.resolve(__dirname, './upload.html')
     const data = await fs.readFile(pathname)
-    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.writeHead(200, { 'Content-Type': 'text/html' })
     response.end(data)
   },
   'POST /upload': async (request, response: http.ServerResponse) => {
@@ -63,27 +76,36 @@ const routes: Record<string, http.RequestListener> = {
       const [fields, files] = await form.parse(request)
       const { avatar } = firstValues(form, files)
       const { fname, lname } = firstValues(form, fields)
-      const oldPath = avatar?.filepath
-      const newPath = path.resolve(__dirname, 'images', `${fname}-${lname}-${avatar.originalFilename}`)
+      const oldPath = avatar?.filepath!
+      const newPath = path.resolve(
+        __dirname,
+        'images',
+        `${fname}-${lname}-${avatar?.originalFilename}`,
+      )
       await fs.rename(oldPath, newPath)
       response.end(JSON.stringify({ success: true }))
     } catch (err) {
       response.statusCode = 500
       response.end(JSON.stringify({ success: false, err }))
     }
-  }
+  },
 }
 
-const getRequestBody: (request: http.IncomingMessage) => Promise<Buffer> = (request) => {
+const getRequestBody: (request: http.IncomingMessage) => Promise<Buffer> = (
+  request,
+) => {
   return new Promise((resolve, reject) => {
     const body: any[] = []
-    request.on('data', chunk => {
-      body.push(chunk)
-    }).on('end', () => {
-      resolve(Buffer.concat(body))
-    }).on('error', err => {
-      reject(err)
-    })
+    request
+      .on('data', (chunk) => {
+        body.push(chunk)
+      })
+      .on('end', () => {
+        resolve(Buffer.concat(body))
+      })
+      .on('error', (err) => {
+        reject(err)
+      })
   })
 }
 
@@ -95,17 +117,17 @@ http
     const routeHandler = routes[routeKey]
 
     if (routeHandler) {
-      routeHandler(request, response);
+      routeHandler(request, response)
     } else {
-      response.writeHead(404, { 'Content-Type': 'text/plain' });
-      response.end('Not Found');
+      response.writeHead(404, { 'Content-Type': 'text/plain' })
+      response.end('Not Found')
     }
 
-    response.on("error", (err) => {
+    response.on('error', (err) => {
       console.error(err)
     })
 
-    request.on("error", (err) => {
+    request.on('error', (err) => {
       console.error(err)
       response.statusCode = 400
       response.end()
