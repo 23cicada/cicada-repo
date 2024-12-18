303331643810510204fde183be20f2f27276ba11
cicada <hey.changjun@gmail.com>
2024-12-18 23:42:56 +0800

    Add nodejs-basic typescript-blog

---
+++ apps/nodejs-basic/README.md
@@ -0,0 +1,11 @@
+# `@repo/nodejs-basic`
+
+## [Running TypeScript Natively](https://nodejs.org/en/learn/typescript/run-natively#running-typescript-natively)
+
+`--experimental-strip-types`
+
+`--experimental-transform-types`
+
+```shell
+node --experimental-strip-types --experimental-transform-types --disable-warning=ExperimentalWarning xxx.ts
+```
---
+++ apps/typescript-blog/src/infer-keyword.ts
@@ -0,0 +1,105 @@
+// https://blog.logrocket.com/understanding-infer-typescript/
+
+function describePerson(person: {
+  name: string
+  age: number
+  hobbies: [string, string] // tuple
+}) {
+  return `${person.name} is ${person.age} years old and loves ${person.hobbies.join(" and  ")}.`
+}
+const alex = {
+  name: "Alex",
+  age: 20,
+  hobbies: ["walking", "cooking"],
+}
+// describePerson(alex)
+// Type string[] is not assignable to type [string, string]
+
+type GetFirstArgumentOfAnyFunction<T> = T extends (
+  first: infer FirstArgument,
+  ...args: any[]
+) => any
+  ? FirstArgument
+  : never
+const alex2: GetFirstArgumentOfAnyFunction<typeof describePerson> = {
+  name: "Alex",
+  age: 20,
+  hobbies: ["walking", "cooking"],
+}
+describePerson(alex2)
+
+// type StringFromType<T> = T extends string ? 'string' : never
+// type lorem = StringFromType<'lorem ipsum'> // 'string'
+// type ten = StringFromType<10> // never
+
+type StringFromType<T> = T extends string
+  ? "string"
+  : T extends boolean
+    ? "boolean"
+    : T extends Error
+      ? "error"
+      : never
+type lorem = StringFromType<"lorem ipsum"> // 'string'
+type isActive = StringFromType<false> // 'boolean'
+type unassignable = StringFromType<TypeError> // 'error'
+
+type NullableString = string | null | undefined
+// type NonNullable<T> = T extends null | undefined ? never : T
+type CondUnionType = NonNullable<NullableString> // evalutes to `string`
+// string | never => string
+// string ∪ ∅ => string
+// string | any => any
+// string ∪ U => any
+
+type stringLoop = string extends null | undefined ? never : string // string
+type nullLoop = null extends null | undefined ? never : null // never
+type undefinedLoop = undefined extends null | undefined ? never : undefined // never
+type ReturnUnion = stringLoop | nullLoop | undefinedLoop // string
+// string ∪ ∅ ∪ ∅ => string
+
+// type Extract<T, U> = T extends U ? T : never
+type T0 = Extract<"a" | "b" | "c", "a" | "f"> // "a"
+type T1 = Extract<"a" | "b" | "c", "a" | "f" | "c"> // "a" | "c"
+
+/**
+ * infer 关键字是对条件类型的补充，不能在 extends 子句之外使用。
+ * 在条件类型中，infer 用于在约束中声明一个类型变量，以便在条件类型的 extends 子句中动态捕获类型。
+ *
+ * 它首先通过检查类型参数 (T) 是否是一个函数，在检查过程中，
+ * 将返回类型赋值为一个变量 infer R，如果检查通过，就返回这个变量。
+ *
+ * 这主要用于访问和使用无法直接获取的类型。
+ */
+type a = ReturnType<() => void> // void
+type b = ReturnType<() => string | number> // string | number
+type c = ReturnType<() => any> // any
+
+// type ReturnType<T> = T extends (...args: any[]) => infer R ? R : any
+
+type ExtractStringType<T> = T extends `${infer U}` ? U : never
+
+type ExtractNumberType<T> = T extends `${infer U}`
+  ? `${U}` extends `${number}`
+    ? U
+    : never
+  : never
+
+type ExtractArrayElementType<T extends readonly any[]> =
+  T extends readonly (infer U)[] ? U : never
+
+type Flatten<T> = T extends Array<infer U> ? Flatten<U> : T
+
+type NestedArray = number[][][]
+
+type Flat = Flatten<NestedArray>
+
+type TupleToUnion<T extends any[]> = T extends [infer U, ...infer Rest]
+  ? U | TupleToUnion<Rest>
+  : never
+
+function getTupleHead<T extends any[]>(tuple: [...T]): TupleToUnion<T> {
+  return tuple[0]
+}
+
+const result1 = getTupleHead(["hello", 42]) // result1 is inferred as string | number
+const result2 = getTupleHead([true, false, true]) // result2 is inferred as boolean
