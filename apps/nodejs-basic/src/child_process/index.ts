7e0ad575325f3ba2e7c1b0c1a12d33bde5fffa70
cicada-c <hey.changjun@gmail.com>
2025-01-02 17:15:25 +0800

    Working on "nodejs-basic"

---
+++ apps/nodejs-basic/src/child_process/index.ts
@@ -0,0 +1,20 @@
+import child_process from "node:child_process"
+import util from "node:util"
+
+child_process.execSync("ls -l src/*.ts", { encoding: "utf-8" })
+child_process.execFileSync("ls", ["-l", "src/"], { encoding: "utf-8" })
+
+const execPromisify = util.promisify(child_process.exec)
+
+function parallelExec(commands: string[]) {
+  const promises = commands.map((command) =>
+    execPromisify(command, { encoding: "utf-8" }),
+  )
+
+  return void Promise.all(promises).then((outputs) =>
+    outputs.map((out) => out.stdout),
+  )
+}
+
+const execFile = child_process.execFile("ls -l src/")
+const spawn = child_process.spawn("ls -l src/")
