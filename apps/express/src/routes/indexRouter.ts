f4631e143312680cdc35e6a5f4f65a6ae05697de
cicada <hey.changjun@gmail.com>
2025-02-04 20:45:09 +0800

    'Express - Views' Completed

--- apps/express/src/routes/indexRouter.ts
+++ apps/express/src/routes/indexRouter.ts
@@ -2,18 +2,23 @@ import { Router } from "express"
 
 const indexRouter = Router()
 
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+]
+
+const users = ["Rose", "Cake", "Biff"]
+
 indexRouter.get("/", (req, res) => {
   res.render("index", {
     message: "EJS rocks!",
-    links: [
-      { href: "/", text: "Home" },
-      { href: "about", text: "About" },
-    ],
+    links,
+    users,
   })
 })
 
 indexRouter.get("/about", (req, res) => {
-  res.send("About")
+  res.render("about")
 })
 
 indexRouter
