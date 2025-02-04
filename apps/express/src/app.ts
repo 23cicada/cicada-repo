6fe5564b7d2b9404fe8b9ac7763e1d768330b191
cicada <cicada>
2025-02-04 20:45:09 +0800

    'Express - Views' Completed

--- apps/express/src/app.ts
+++ apps/express/src/app.ts
@@ -1,12 +1,11 @@
 import express from "express"
-import type { ErrorRequestHandler } from "express"
 import authorRouter from "./routes/authorRouter.ts"
 import bookRouter from "./routes/bookRouter.ts"
 import indexRouter from "./routes/indexRouter.ts"
-import type { CustomError } from "./interface.js"
 import path from "node:path"
 import errorHandler from "./controllers/errorController.ts"
 
+const assetsPath = path.join(import.meta.dirname, "public")
 const app = express()
 
 app.use("/authors", authorRouter)
@@ -17,6 +16,7 @@ app.use(errorHandler)
 
 app.set("views", path.join(import.meta.dirname, "views"))
 app.set("view engine", "ejs")
+app.use(express.static(assetsPath))
 
 const PORT = process.env.PORT ?? 3001
 
