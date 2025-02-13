8ea4fbac4165a21593a605667d3e8d0857e96610
cicada-c <hey.changjun@gmail.com>
2025-02-13 18:05:25 +0800

    Get started - docker

--- apps/docker-gs/getting-started-todo-app/backend/src/routes/getGreeting.js
+++ apps/docker-gs/getting-started-todo-app/backend/src/routes/getGreeting.js
@@ -1,8 +1,11 @@
-const GREETING = 'Hello world!';
+const GREETINGS = [
+    "Welcome!",
+    "All hands on deck!",
+    "Charting the course ahead!",
+];
 
 module.exports = async (req, res) => {
-    debugger;
     res.send({
-        greeting: GREETING,
+        greeting: GREETINGS[ Math.floor( Math.random() * GREETINGS.length )],
     });
 };
