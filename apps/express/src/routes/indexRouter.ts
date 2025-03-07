74fcf48ed487e43dfa259b2d210eca2ecf01f39a
cicada-co. <hey.changjun@gmail.com>
2025-03-07 17:21:25 +0800

    Add vite

--- apps/express/src/routes/indexRouter.ts
+++ apps/express/src/routes/indexRouter.ts
@@ -1,4 +1,13 @@
 import { Router } from "express"
+import fs from "node:fs/promises"
+
+const isProduction = process.env.NODE_ENV === "production"
+const base = process.env.BASE ?? "/"
+const ABORT_DELAY = 10000
+
+const templateHtml = isProduction
+  ? await fs.readFile("./dist/client/index.html", "utf-8")
+  : ""
 
 const indexRouter = Router()
 
