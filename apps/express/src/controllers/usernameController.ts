ab4fe89e0afd355bca1517bc3db27e07aad720f8
cicada-c <hey.changjun@gmail.com>
2025-05-15 18:10:50 +0800

    NodeJS Course: Using PostgreSQL

--- apps/express/src/controllers/usernameController.ts
+++ apps/express/src/controllers/usernameController.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from "express"
 import asyncHandler from "express-async-handler"
+import { body, validationResult } from "express-validator"
+import { ValidationError } from "../errors/ValidationError.ts"
 import * as db from "../db/queries.ts"
 
 const getUsernames = asyncHandler(async (_, res: Response) => {
@@ -7,10 +9,22 @@ const getUsernames = asyncHandler(async (_, res: Response) => {
   res.success(usernames)
 })
 
-const createUsername = asyncHandler(async (req: Request, res: Response) => {
-  const { username } = req.body
-  await db.insertUsername(username)
-  res.redirect("/")
-})
+const createUsername = [
+  body("username")
+    .trim()
+    .isAlpha()
+    .withMessage(`Username must only contain letters.`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Username must be between 1 and 10 characters.`),
+  asyncHandler(async (req: Request, res: Response) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      throw new ValidationError(errors.array().toString())
+    }
+    const { username } = req.body
+    await db.insertUsername(username)
+    res.success()
+  }),
+]
 
 export { getUsernames, createUsername }
--- apps/express/types/global.d.ts
+++ apps/express/types/global.d.ts
@@ -1,7 +1,7 @@
 declare global {
   namespace Express {
     interface Response {
-      success: <T = unknown>(data: T, message?: string, statusCode?: number) => void;
+      success: <T = unknown>(data?: T, message?: string, statusCode?: number) => void;
       error: (code: string, message: string, statusCode?: number, details?: unknown) => void;
     }
   }
